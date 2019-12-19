9aa9e413be399740abeae79b409859c66fb970f9
kagan <kubilaykagankomurcu@gmail.com>
2019-12-19 14:47:28 +0300

    Bow shocks added

--- plots_and_evaluations.py
+++ plots_and_evaluations.py
@@ -4,17 +4,22 @@ import plotly.graph_objects as go
 import csv
 
 
+def magnetopause_distance(n, V):
+    mp = 1.67 * 10 ** (-27)
+    B_0 = 0.36  # (gauss)
+    nu_0 = 4 * np.pi * 10 ** (-7)
+    return ((B_0 ** 2) / (nu_0 * mp * n * V ** 2)) ** (1 / 6)
 
 
+def BowShock_distance(R_mp, gamma = 5/3):
+    1.1*(gamma-1)/(gamma+1)*R_mp
 
-
-
-def magnetopause_distance()
 
 def dynamic_pressure(n, V):
-    mp = 1.67 * 10**(-27)
-    n = n * 10**6
-    return 0.5*n*mp*V**2
+    mp = 1.67 * 10 ** (-27)
+    n = n * 10 ** 6
+    return 0.5 * n * mp * V ** 2
+
 
 def eV_to_Kelvin(Temperature):
     return Temperature * 11600
