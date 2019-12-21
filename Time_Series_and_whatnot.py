552a5a95a83894b09ff3b64dc4f759e425e96c43
Kaan <kubilaykagankomurcu@gmail.com>
2019-12-21 22:07:51 +0300

    bad passings fixed from the data

--- Time_Series_and_whatnot.py
+++ Time_Series_and_whatnot.py
@@ -25,11 +25,6 @@ axes[4].set(ylabel='Bx (nT)')
 axes[5].set(ylabel='By (nT)')
 axes[6].set(ylabel='Bz (nT)')
 
-
-
-
-
-
 for ax in axes.flat:
     timelist = ['20.00', '21.00', '22.00', '23.00', '00.00', '01.00']
     ax.yaxis.set_major_locator(AutoLocator())
@@ -46,25 +41,45 @@ for ax in axes.flat:
 plt.show()
 
 
+
+
+
+
 '''
+fig, axes = plt.subplots(nrows=7, ncols=1, sharex=True)
+axes[0].set_title('THEMIS-C')
 
-axs[0, 1].plot(x, y, 'tab:orange')
-axs[0, 1].set_title('Axis [0,1]')
-axs[1, 0].plot(x, -y, 'tab:green')
-axs[1, 0].set_title('Axis [1,0]')
-axs[1, 1].plot(x, -y, 'tab:red')
-axs[1, 1].set_title('Axis [1,1]')'''
-'''data.groupby(data.time.dt.hour).Vmom.plot()
-data.groupby(data.time.dt.hour).Tmom.plot()
-data.groupby(data.time.dt.hour).nmom.plot()
-data.groupby(data.time.dt.hour).BmagFGM.plot()
-data.groupby(data.time.dt.hour).BxFGM.plot()
-data.groupby(data.time.dt.hour).ByFGM.plot()
-data.groupby(data.time.dt.hour).BzFGM.plot()'''
+data.groupby(data.time.dt.hour).Vmom.plot(ax=axes[0])
+data.groupby(data.time.dt.hour).Tmom.plot(ax=axes[1])
+data.groupby(data.time.dt.hour).nmom.plot(ax=axes[2])
+data.groupby(data.time.dt.hour).BmagFGM.plot(ax=axes[3])
+data.groupby(data.time.dt.hour).BxFGM.plot(ax=axes[4])
+data.groupby(data.time.dt.hour).ByFGM.plot(ax=axes[5])
+data.groupby(data.time.dt.hour).BzFGM.plot(ax=axes[6])
 
+axes[0].set(ylabel='Vtot (km/s)')
+axes[1].set(ylabel='T (K)')
+axes[2].set(ylabel='n (cm^(-3))')
+axes[3].set(ylabel='Btot (nT)')
+axes[4].set(ylabel='Bx (nT)')
+axes[5].set(ylabel='By (nT)')
+axes[6].set(ylabel='Bz (nT)')
 
+timelist = ['20.00', '21.00', '22.00', '23.00', '00.00', '01.00']
 
+for ax in axes.flat:
+    ax.yaxis.set_major_locator(AutoLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
+    ax.yaxis.set_minor_formatter(NullFormatter())
+    ax.xaxis.set_major_locator(LinearLocator(6))
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.xaxis.set_major_formatter(FixedFormatter(timelist))
+    ax.xaxis.set_minor_formatter(NullFormatter())
+    ax.set(xlabel='TIME')
+plt.show()
 
+'''
 
 
 
