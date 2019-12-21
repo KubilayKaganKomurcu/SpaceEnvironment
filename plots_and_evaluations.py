fc19a0b096c2146e230f3086019f7fa1dda5b50e
kagan <kubilaykagankomurcu@gmail.com>
2019-12-21 16:56:33 +0300

    Had to commit with an unfinished code

--- plots_and_evaluations.py
+++ plots_and_evaluations.py
@@ -7,28 +7,76 @@ import csv
 with open("new_data.csv", "r") as Big_data:
     Big_data_reader = csv.reader(Big_data)
 
+    Vswe = []
+    Tswe = []
+    nswe = []
+    BmagMFI = []
+    BxMFI = []
+    ByMFI = []
+    BzMFI = []
+    Vmom = []
+    Tmom = []
+    nmom = []
+    BmagFGM = []
+    BxFGM = []
+    ByFGM = []
+    BzFGM = []
+    TimeData = []
     for row in Big_data_reader:
-        data1 = row[0]
-        data5 = row[5]
-        data6 = row[6]
-        data7 = row[7]
-        data4 = row[4]
+        TimeData.append(row[0])
+        Vswefloat = float(row[1])
+        Tswefloat = float(row[2])
+        nswefloat = float(row[3])
+        BmagMFIfloat = float(row[4])
+        BxMFIfloat = float(row[5])
+        ByMFIfloat = float(row[6])
+        BzMFIfloat = float(row[7])
+        Vmomfloat = float(row[8])
+        Tmomfloat = float(row[9])
+        nmomfloat = float(row[10])
+        BmagFGMfloat = float(row[11])
+        BxFGMfloat = float(row[12])
+        ByFGMfloat = float(row[13])
+        BzFGMfloat = float(row[14])
+
+        Vswe.append(Vswefloat)
+        Tswe.append(Tswefloat)
+        nswe.append(nswefloat)
+        BmagMFI.append(BmagMFIfloat)
+        BxMFI.append(BxMFIfloat)
+        ByMFI.append(ByMFIfloat)
+        BzMFI.append(BzMFIfloat)
+        Vmom.append(Vmomfloat)
+        Tmom.append(Tmomfloat)
+        nmom.append(nmomfloat)
+        BmagFGM.append(BmagFGMfloat)
+        BxFGM.append(BxFGMfloat)
+        ByFGM.append(ByFGMfloat)
+        BzFGM.append(BzFGMfloat)
+    print(np.max(BmagMFI))
+    print(np.max(BxMFI))
+    print(np.max(ByMFI))
+    print(np.max(BzMFI))
+    print(np.min(BmagMFI))
+    print(np.min(BxMFI))
+    print(np.min(ByMFI))
+    print(np.min(BzMFI))
 
     fig = plt.figure()
     ax1 = fig.add_axes([0.1, 0.04, 0.25, 0.25],
-                       xticklabels=[], ylim=(-1.2, 1.2))
+                       xticklabels=[], ylim=(-3, 2))
     ax2 = fig.add_axes([0.1, 0.27, 0.25, 0.25],
-                       ylim=(-1.2, 1.2))
+                       ylim=(-3, 2))
     ax3 = fig.add_axes([0.1, 0.50, 0.25, 0.25],
-                       xticklabels=[], ylim=(-1.2, 1.2))
-    ax4= fig.add_axes([0.1, 0.73, 0.25, 0.25],
-                       ylim=(-1.2, 1.2))
+                       xticklabels=[], ylim=(-3, 2))
+    ax4 = fig.add_axes([0.1, 0.73, 0.25, 0.25],
+                       ylim=(0, 2))
 
-    x = data1
-    ax1.plot(data5)
-    ax2.plot(data6)
-    ax3.plot(data7)
-    ax4.plot(data4)
+    x = TimeData
+    ax1.plot(BxMFI)
+    ax2.plot(ByMFI)
+    ax3.plot(BzMFI)
+    ax4.plot(BmagMFI)
 
     plt.show()
 
