8bf1b9af23ef6e5a1166f7a18afba23ae3b75347
kagan <kubilaykagankomurcu@gmail.com>
2019-12-23 09:20:40 +0300

    distances.py constructed

--- plots_and_evaluations.py
+++ plots_and_evaluations.py
@@ -30,47 +30,10 @@ def dynamic_pressure(n, V):
     mp = 1.67 * 10 ** (-27)
     n = n * 10 ** 6
     V = V * 1000
-    return 2 * n * mp * V ** 2
+    return (2 * n * mp * V ** 2) * 10**9 # nanoTesla
 
 
 def eV_to_Kelvin(Temperature):
     return Temperature * 11600
 
 
-with open("new_data.csv", "r") as GrandData:
-    GrandData_reader = csv.reader(GrandData)
-    next(GrandData_reader)
-    list_d_MP = []
-    list_d_BS = []
-    list_P_dyn = []
-
-    for line in GrandData_reader:
-        V = line[1]
-        n = line[3]
-
-        V = float(V)
-        n = float(n)
-
-        if abs(V) > 10 ** 10:
-            continue
-        if abs(n) > 10 ** 10:
-            continue
-
-        d_MP = magnetopause_distance(n, V)
-        list_d_MP.append(d_MP)
-
-        d_BS = BowShock_distance(d_MP)
-        list_d_BS.append(d_BS)
-
-        P_dyn = dynamic_pressure(n, V)
-        list_P_dyn.append(P_dyn)
-
-with open("distances.csv", "w") as new_file:
-    fieldnames = ['MPAUSEdistance', 'BowShockDistance', "DynamicPressure"]
-
-    writer = csv.DictWriter(new_file, fieldnames=fieldnames)
-    writer.writeheader()
-
-    for i in range(len(list_d_BS)):
-        writer.writerow({'MPAUSEdistance': list_d_MP[i], 'BowShockDistance': list_d_BS[i],
-                         "DynamicPressure": list_P_dyn[i]})
